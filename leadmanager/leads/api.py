ba6799326435fca773a70d8ceba9f95104336218
Lady_Proger <facktartar@mail.com>
2020-05-23 17:32:27 +0400

    critical fix for requests

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -4,15 +4,9 @@ from .serializers import LeadSerializer
 
 # Lead Viewset
 
-
 class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
     permission_classes = [
-        permissions.IsAuthenticated,
+        permissions.AllowAny
     ]
     serializer_class = LeadSerializer
-
-    def get_queryset(self):
-        return self.request.user.leads.all()
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
